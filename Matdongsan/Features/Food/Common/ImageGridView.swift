6835878f5414d2149edfb56a00820a746d4900e2
sio2whocodes <fltcy2039@naver.com>
2025-06-02 15:53:30 +0900

    File: 폴더 구조 변경

---
+++ Matdongsan/Features/Food/Common/ImageGridView.swift
@@ -0,0 +1,80 @@
+//
+//  ImageGridView.swift
+//  Matdongsan
+//
+//  Created by sujeong on 5/28/25.
+//
+
+import SwiftUI
+
+@available(iOS 18.0, *)
+struct ImageGridView: View {
+    @Binding var isPresented:Bool
+    var images:[String] = ["corn", "launchImage", "corn"]
+    @Binding var selectedId:Int
+    @State var position:ScrollPosition = .init(id: 0)
+
+    var body: some View {
+        VStack {
+            Spacer()
+            
+            HStack(spacing: 8) {
+                Spacer()
+                Text("옥수수")
+                    .padding(6)
+                    .background(Color(uiColor: UIColor(hexCode: "F4F4F4")).opacity(0.1))
+                    .cornerRadius(10)
+                    .font(.callout)
+                    .foregroundStyle(.white)
+
+                Button {
+                    isPresented.toggle()
+                } label: {
+                    Image(systemName: "multiply.circle")
+                        .tint(.mdCoolgray20)
+                        .imageScale(.large)
+                }
+            }
+            .padding([.bottom, .trailing], 20)
+            
+            
+            ScrollView(.horizontal) {
+                HStack (spacing: 10) {
+                    ForEach(Array(images.enumerated()), id: \.offset) { i, image in
+                        Image(image)
+                            .resizable()
+                            .aspectRatio(contentMode: .fill)
+                            .frame(width: UIScreen.main.bounds.width-90, height: UIScreen.main.bounds.width-90)
+                            .cornerRadius(10)
+                            .shadow(color: .black.opacity(0.2), radius: 8, x: 0, y: 4)
+                            .scrollTransition { content, phase in
+                                content
+                                    .scaleEffect(y: phase.isIdentity ? 1 : 0.8)
+                                    .opacity(phase.isIdentity ? 1 : 0.8)
+                            }
+                            .id(i)
+                    }
+                }
+                .scrollTargetLayout()
+            }
+            .contentMargins(.horizontal, 45, for: .scrollContent)
+            .scrollTargetBehavior(.viewAligned)
+            .scrollIndicators(.hidden)
+            .scrollPosition($position)
+            
+            Spacer()
+        }
+        .onAppear {
+            position.scrollTo(id: selectedId)
+        }
+    }
+}
+
+#Preview {
+    if #available(iOS 18.0, *) {
+        ImageGridView(isPresented: .constant(true), selectedId: .constant(0))
+            .background(Color(uiColor: UIColor(hexCode: "21272A")).opacity(0.4))
+    } else {
+        // Fallback on earlier versions
+    }
+}
---
+++ Matdongsan/Features/Food/FoodStory/FoodPlaceCell.swift
@@ -0,0 +1,152 @@
+//
+//  FoodPlaceCell.swift
+//  Matdongsan
+//
+//  Created by sujeong on 5/8/25.
+//
+
+import SwiftUI
+
+struct FoodPlaceCell: View {
+    private var userId:String = "" // current user 어떻게 할까
+    private var nickname: String = "도란도란3" // author
+    private var createdAt: String = "2025.05.09"
+    
+    private var place_name:String = "토오베"
+    private var category:String = "차 전문점"
+    private var address:String = "서울특별시 종로구 관훈동 118-36 3층"
+    private var review:String = "여름시즌에만 한정 판매하는 옥수수 빙수를 꼭 먹어보세요"
+    
+    @State private var isLiked: Bool = false
+    private var likeCnt: Int = 0
+    
+    var images:[String] = ["corn", "launchImage", "corn"]
+    @State var isClicked = false
+    
+    var body: some View {
+        VStack {
+            HStack (spacing: 8) {
+                Image("user-profile")
+                    .frame(width: 36, height: 36)
+                VStack (alignment: .leading, spacing: 2) {
+                    Text(nickname)
+                        .foregroundStyle(.mdCoolgray90)
+                        .font(.system(size: 15))
+                    Text(createdAt)
+                        .foregroundStyle(.mdCoolgray30)
+                        .fontWeight(.light)
+                        .font(.system(size: 13))
+                }
+                
+                Spacer()
+                
+                Image("place-icon")
+                    .frame(width: 24, height: 24)
+            }
+            
+            Divider()
+            
+            VStack (alignment: .leading, spacing: 10) {
+                
+                HStack {
+                    VStack (alignment: .leading, spacing: 8) {
+                        HStack (spacing: 8) {
+                            Text(place_name)
+                                .font(.headline)
+                                .fontWeight(.semibold)
+                                .padding(6)
+                                .background(.mdGreen10)
+                                .cornerRadius(8)
+                            
+                            Text(category)
+                                .foregroundStyle(.mdCoolgray90)
+                                .font(.system(size: 15))
+                        }
+                        
+                        Text(address)
+                            .font(.footnote)
+                            .fontWeight(.thin)
+                            .foregroundStyle(.mdCoolgray60)
+                        
+                        Spacer()
+                        
+                    }
+                    
+                    Spacer()
+                    
+                    Image("cornfirst")
+                        .resizable()
+                        .frame(width: 78, height: 78)
+                        .onTapGesture {
+                            isClicked.toggle()
+                        }
+                        .popover(isPresented: $isClicked) {
+                            if #available(iOS 18.0, *) {
+                                ImageGridView(isPresented: $isClicked, selectedId: .constant(0))
+                                    .presentationBackground(Color(uiColor: UIColor(hexCode: "21272A")).opacity(0.4))
+                                    .presentationCompactAdaptation(.fullScreenCover)
+                            } else {
+                                // Fallback on earlier versions
+                            }
+                        }
+                }
+                .frame(height: 78) // temp
+            }
+            
+            VStack (alignment: .leading, spacing: 8) {
+                Text("식당 한줄평")
+                    .font(.system(size: 14))
+
+                Text(review)
+                    .font(.system(size: 15))
+                    .fontWeight(.thin)
+                    .foregroundStyle(.mdCoolgray90)
+
+            }
+            .padding(.horizontal, 16)
+            .padding(.vertical, 10)
+            .frame(maxWidth: .infinity, alignment: .leading)
+            .background(.mdCoolgray10.opacity(0.5))
+            .cornerRadius(8)
+            
+            Divider()
+            
+            HStack (alignment: .center, spacing: 4) {
+                Button {
+                    // TODO
+                    self.isLiked.toggle()
+                } label: {
+                    isLiked ? Image(systemName: "heart.fill")
+                        .imageScale(.large)
+                        .foregroundStyle(.mdCyan40) : Image(systemName: "heart")
+                        .imageScale(.large)
+                        .foregroundStyle(.mdCoolgray50)
+                }
+                
+                Text("\(likeCnt)")
+                    .font(.callout)
+                    .foregroundStyle(.mdCoolgray50)
+                
+                Spacer()
+                
+                Button {
+                    // TODO
+                } label: {
+                    Image("vertical-ellipsis")
+                }
+            }
+            .padding(.vertical, 1)
+
+        }
+        .padding(.horizontal, 16)
+        .padding(.vertical, 12)
+        .background(.white)
+        .cornerRadius(16)
+        .shadow(color: .mdCoolgray10, radius: 8, x: 1, y: 2)
+        
+    }
+}
+
+#Preview {
+    FoodPlaceCell()
+}
