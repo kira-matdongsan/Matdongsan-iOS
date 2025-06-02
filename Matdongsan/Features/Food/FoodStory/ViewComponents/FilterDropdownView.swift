8760a4a898c9061badbb6aa29b6e5a7a6baeb060
sio2whocodes <fltcy2039@naver.com>
2025-06-02 15:50:57 +0900

    File: 폴더구조 변경

---
+++ Matdongsan/Features/Food/FoodStory/ViewComponents/FilterDropdownView.swift
@@ -0,0 +1,62 @@
+//
+//  FilterDropdownView.swift
+//  Matdongsan
+//
+//  Created by sujeong on 5/27/25.
+//
+
+import SwiftUI
+
+struct FilterDropdownView: View {
+    
+    var categories:[String] = ["전체", "레시피", "플레이스","제철기록"]
+    @Binding var selectedIdx:Int
+    @Binding var isPresenting:Bool
+    @Binding var selectedItem:String
+    var storyIconMap:[String:String] = ["레시피":"recipe-line-icon", "플레이스":"place-line-icon", "제철기록":"record-line-icon"]
+    
+    var body: some View {
+        LazyVGrid(columns: [GridItem()], spacing: 0) {
+            ForEach(Array(categories.enumerated()), id: \.offset) { i, category in
+                Button {
+                    selectedIdx = i
+                    selectedItem = categories[selectedIdx]
+                    withAnimation(.easeIn(duration: 0.07), {
+                        isPresenting = false
+                    })
+                } label: {
+                    HStack {
+                        if i != 0 {
+                            Image(storyIconMap[category] ?? "")
+                        }
+                        Text(category)
+                            .font(.system(size: 15))
+                            .foregroundStyle(i == selectedIdx ? .mdCoolgray80 : .mdCoolgray60)
+                        Spacer()
+                        Image(i == selectedIdx ? "checked_icon" : "unchecked_icon")
+                    }
+                    .frame(height: 28)
+                    .padding(.horizontal, 8)
+                    .padding(.vertical, 4)
+                }
+
+                if i != categories.count-1 {
+                    Rectangle()
+                        .frame(height:1)
+                        .foregroundStyle(.mdCoolgray20)
+                }
+            }
+            
+        }
+        .background()
+        .frame(width:120)
+        .cornerRadius(8)
+        .clipped()
+        .shadow(color: .mdCoolgray20, radius: 10, x: 0, y: 4)
+        
+    }
+}
+
+#Preview {
+    FilterDropdownView(selectedIdx: .constant(0), isPresenting: .constant(true), selectedItem: .constant("전체"))
+}
---
+++ Matdongsan/Features/Food/FoodStory/ViewComponents/MenuButtonView.swift
@@ -0,0 +1,59 @@
+//
+//  MenuButtonView.swift
+//  Matdongsan
+//
+//  Created by sujeong on 5/29/25.
+//
+
+import SwiftUI
+
+struct MenuButtonView: View {
+    @Binding var selectedSortIdx:Int
+    @Binding var selectedSort:String
+    @Binding var isPresenting:Bool
+    var sort:String
+    var index:Int
+    @State var isSelected:Bool = false
+    
+    var body: some View {
+        Button {
+            //
+            selectedSortIdx = index
+            selectedSort = sort
+            isSelected = true
+            withAnimation(.easeIn(duration: 0.07), {
+                isPresenting = false
+            })
+        } label: {
+            ZStack {
+                if isSelected {
+                    Rectangle()
+                        .fill(.mdCoolgray10)
+                }
+                HStack {
+                    if selectedSortIdx == index {
+                        Text(sort)
+                            .font(.system(size: 15))
+                            .foregroundStyle(.mdCoolgray80)
+                        Spacer()
+                        Image("checked_icon")
+                    } else {
+                        Text(sort)
+                            .font(.system(size: 15))
+                            .foregroundStyle(.mdCoolgray60)
+                        Spacer()
+                        Image("unchecked_icon")
+                    }
+                }
+                .frame(height: 28)
+                .padding(.horizontal, 8)
+                .padding(.vertical, 4)
+            }
+            
+        }
+    }
+}
+
+#Preview {
+    MenuButtonView(selectedSortIdx: .constant(0), selectedSort: .constant("최신순"), isPresenting: .constant(true), sort: "최신순", index: 0)
+}
