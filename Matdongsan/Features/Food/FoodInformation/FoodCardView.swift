d085a0f95d34447dd28c6feb98494bf2baba7d4e
sio2whocodes <fltcy2039@naver.com>
2025-07-07 15:01:56 +0900

    Feat: card flip animation 원근감, scale 조정

--- Matdongsan/Features/Food/FoodInformation/FoodCardView.swift
+++ Matdongsan/Features/Food/FoodInformation/FoodCardView.swift
@@ -14,6 +14,7 @@ struct FoodCardView: View {
     @State private var isLiked: Bool = false
     
     @State private var flip: Bool = true
+    @State private var scale: CGFloat = 1
     
     private var description: String = "7월 둘째주 선정된 제철음식은 바로 ‘옥수수' 에요. 더워지기 시작하는 초여름에는 아삭아삭한 초당옥수수부터 땡볕 더위엔 쫀득쫀득한 찰옥수수까지 다양한 식감으로 여름 대표 간식으로 불리운답니다."
     
@@ -63,7 +64,7 @@ struct FoodCardView: View {
                 .background()
                 .cornerRadius(24)
                 .shadow(color: .mdCoolgray20, radius: 4, x: 1, y: 2)
-                .rotation3DEffect(Angle(degrees: 180), axis: (x: 0.0, y: 1.0, z: 0.0))
+                .rotation3DEffect(Angle(degrees: 180), axis: (x: 0.0, y: 1.0, z: 0.0), perspective: 0.2)
             } else {
                 // card back
                 ZStack {
@@ -89,7 +90,7 @@ struct FoodCardView: View {
                     }
                     .padding(16)
                     .frame(maxWidth: .infinity, alignment: .leading)
-
+                    
                 }
                 .background()
                 .cornerRadius(24)
@@ -97,10 +98,25 @@ struct FoodCardView: View {
             }
         }
         .padding(.horizontal, 16)
-        .rotation3DEffect(.degrees(flip ? 180 : 0), axis: (x: 0, y: 1, z: 0))
+        .scaleEffect(scale)
+        .rotation3DEffect(.degrees(flip ? 180 : 0), axis: (x: 0, y: 1, z: 0), perspective: 0.2)
         .animation(.easeInOut(duration: 0.4), value: flip)
         .onTapGesture {
-            flip.toggle()
+            // Step 1: scale down
+            withAnimation(.easeInOut(duration: 0.15)) {
+                scale = 0.8
+            }
+            
+            // Step 2: rotate after slight delay
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.15) {
+                
+                // Step 3: scale back up
+                withAnimation(.easeInOut(duration: 0.15)) {
+                    flip.toggle()
+                    scale = 1.0
+                }
+            }
+
         }
     }
 }
