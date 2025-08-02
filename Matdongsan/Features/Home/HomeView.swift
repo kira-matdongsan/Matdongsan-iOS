f689735e797f0ec0a7b7d59bda29676d0337426d
sio2whocodes <fltcy2039@naver.com>
2025-08-02 16:19:46 +0900

    Feat: 홈화면 투표결과 UI & Model

--- Matdongsan/Features/Home/HomeView.swift
+++ Matdongsan/Features/Home/HomeView.swift
@@ -20,6 +20,17 @@ struct HomeView: View {
     @State var discovered:Bool = false
     var weeks:[Int] = [4,5,6,7,8,9,10]
     
+    // vote
+    let dummyVoteResult = VoteResult(
+        month: 8,
+        results: [
+            VoteItem(id: UUID().uuidString, name: "갈치", percentage: 17.4, count: 14, isFirst: false),
+            VoteItem(id: UUID().uuidString, name: "옥수수", percentage: 65.3, count: 53, isFirst: true),
+            VoteItem(id: UUID().uuidString, name: "자두", percentage: 12.7, count: 10, isFirst: false),
+            VoteItem(id: UUID().uuidString, name: "한치", percentage: 4.6, count: 4, isFirst: false)
+        ]
+    )
+    
     var body: some View {
         
         VStack (spacing: 0) {
@@ -53,9 +64,17 @@ struct HomeView: View {
                     
                     // 주간 제철 기록장
                     WeeklyRecordView()
+                        .padding(.bottom, 16)
+                    
+                    CustomDivider()
+                        .padding(.vertical, 16)
                     
                     // 투표결과
+                    VoteResultView(voteResult: dummyVoteResult) // data temp
                     
+                    CustomDivider()
+                        .padding(.vertical, 16)
+
                     // 제철 시세
                 }
             }
